369dfa07a8163ec69529f2edcbeaabc00bf9ee26
Aime Ramalho <aimeariadne@hotmail.com>
2023-07-12 16:19:27 +0100

    create qiz count

---
+++ src/components/atoms/QuizCount.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const props = defineProps<{
+  count: number
+}>()
+</script>
+
+<template>
+  <p class="quiz-count">{{ count }}</p>
+</template>
+
+<style scoped>
+.quiz-count {
+  @apply
+    text-slate-100
+    font-semibold
+}
+</style>
