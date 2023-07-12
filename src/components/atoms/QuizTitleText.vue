7c9e03b540769342b2758bbe2a924038e5e459f6
Aime Ramalho <aimeariadne@hotmail.com>
2023-07-12 16:19:47 +0100

    create quiz title text

---
+++ src/components/atoms/QuizTitleText.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const props = defineProps<{
+  text: string
+}>()
+
+</script>
+
+<template>
+  <h2 class="quiz-text">{{ text }}</h2>
+</template>
+
+<style scoped>
+.quiz-text {
+  @apply
+  text-slate-200
+    text-center
+    text-4xl
+    p-4
+    border-b-solid
+    border-b-[1px]
+}
+</style>
