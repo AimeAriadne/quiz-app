37eb66fd385e417c886c1fdafa8e33b2d9767c50
Aime Ramalho <aimeariadne@hotmail.com>
2023-07-12 16:21:21 +0100

    create option component

---
+++ src/components/atoms/QuizOption.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+const props = defineProps<{
+  option: string,
+  disabled?: boolean,
+  modelValue: string | null
+}>()
+
+const emit = defineEmits<{
+  (e: 'update:modelValue', value: string): void
+}>()
+</script>
+
+<template>
+  <div class="quiz-option">
+    <input 
+      :id="option"
+      :value="modelValue"
+      :disabled="disabled"
+      type="radio"
+      name="option"  
+      @change="emit('update:modelValue', option)"
+    >
+
+    <label :for="option">{{ option }}</label>
+  </div>
+</template>
+
+<style scoped>
+.quiz-option {
+  @apply
+    text-slate-100
+    text-lg
+    flex
+    gap-2
+}
+</style>
