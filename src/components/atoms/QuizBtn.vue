6be8e2282b4485c254bce74813d217d6e547aac2
Aime Ramalho <aimeariadne@hotmail.com>
2023-07-12 16:19:01 +0100

    create quiz button

---
+++ src/components/atoms/QuizBtn.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+const props = defineProps<{
+  label: string
+}>()
+
+const emit = defineEmits<{
+  (e: 'handleClick'): void
+}>()
+</script>
+
+<template>
+  <button
+    class="quiz-btn"
+    @click="emit('handleClick')"
+  >
+    {{ label }}
+  </button>
+</template>
+
+<style>
+.quiz-btn {
+  @apply
+    text-slate-100
+    bg-blue-700
+    text-base
+    border-0
+    w-28
+    py-2
+}
+
+.quiz-btn:disabled {
+  @apply
+    bg-blue-400
+    cursor-not-allowed
+}
+</style>
