fe3424200b8fe1c0f94338901e5c8da4510bc11e
Aime Ramalho <aimeariadne@hotmail.com>
2023-07-12 16:21:41 +0100

    create component to group options

---
+++ src/components/molecules/QuizOptions.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import QuizOption from '@/components/atoms/QuizOption.vue'
+import { useQuizStore } from '@/stores/quiz';
+const options: string[] = ['s', 'e', 'dd', '22w']
+
+const { selectedOption, toBeAnswered } = useQuizStore()
+
+</script>
+
+<template>
+  <div class="quiz-options" >
+    <QuizOption  
+      v-model="selectedOption"
+      v-for="(option, index) in options"
+      :key="`option-${index}`"
+      :option="option" 
+      :disabled="!toBeAnswered"
+    />
+  </div>
+</template>
