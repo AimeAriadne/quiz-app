99526b440a9c65860253533853ffa7283dae7c5b
Aime Ramalho <aimeariadne@hotmail.com>
2023-07-12 16:20:12 +0100

    create component to show correct or incorrect answer

---
+++ src/components/atoms/QuizCheck.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+const props = defineProps<{
+  correctAnswer: boolean
+}>()
+</script>
+
+<template>
+  <p
+    class="quiz-check"
+    :class="correctAnswer ? 'quiz-correct' : 'quiz-incorrect'"
+  >
+    {{ correctAnswer ? 'Correct!' : 'Incorrect!' }}
+  </p>
+</template>
+
+<style>
+.quiz-check {
+  @apply
+    w-3/6
+}
+
+.quiz-correct {
+  @apply
+    bg-green-500
+    text-green-600
+    border-green-600
+}
+
+.quiz-incorrect {
+  @apply
+    bg-red-500
+    text-red-600
+    border-red-600
+}
+
+.quiz-correct,
+.quiz-incorrect {
+  @apply
+    text-lg
+    text-center
+    font-semibold
+    p-1
+    border-solid
+    bg-opacity-30
+}
+</style>
