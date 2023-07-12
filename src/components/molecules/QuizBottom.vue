c9456e4486bdda700b9fb3ecc28f7754cce11313
Aime Ramalho <aimeariadne@hotmail.com>
2023-07-12 16:21:06 +0100

    create quiz bottom component

---
+++ src/components/molecules/QuizBottom.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import QuizCheck from '@/components/atoms/QuizCheck.vue'
+import QuizCount from '@/components/atoms/QuizCount.vue'
+import QuizBtn from '@/components/atoms/QuizBtn.vue'
+
+import { useQuizStore } from '@/stores/quiz'
+
+const { correctAnswer, quizLengthCount, toBeAnswered } = useQuizStore()
+</script>
+
+<template>
+  <div class="quiz-bottom">
+    <QuizCheck 
+      v-show="!toBeAnswered"
+      :correctAnswer="correctAnswer"
+    />
+    
+    <QuizCount :count="quizLengthCount"/>
+
+    <QuizBtn label="test"/>
+  </div>
+</template>
+
+<style scoped>
+.quiz-bottom {
+  @apply
+    flex
+    items-center
+    justify-between
+}
+
+.quiz-count {
+  @apply
+    text-slate-100
+    font-semibold
+}
+</style>
