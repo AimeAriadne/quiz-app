42bce4a8c0bd6a5418e8efe335fbb20b350ef25c
Aime Ramalho <aimeariadne@hotmail.com>
2023-07-13 14:37:56 +0100

    create result component

---
+++ src/components/molecules/QuizResult.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import QuizTitleText from '@/components/atoms/QuizTitleText.vue'
+import QuizBtn from '@/components/atoms/QuizBtn.vue'
+import { storeToRefs } from 'pinia'
+import { useQuizStore } from '@/stores/quiz'
+
+const emit = defineEmits<{
+  (e: 'handleRestart'): void
+}>()
+
+const { 
+  correctAnswers, 
+  questions, 
+} = storeToRefs(useQuizStore())
+
+</script>
+
+<template>
+  <div class="quiz-result">
+    <QuizTitleText :text="`Your final score is ${correctAnswers} out of ${questions.length }`"/>
+
+    <QuizBtn 
+      label="Restart"
+      type="button"
+      @click="emit('handleRestart')"
+    />
+  </div>
+</template>
+
+<style scoped>
+.quiz-result {
+  @apply
+    flex
+    flex-col
+    items-center
+    gap-4
+}
+</style>
