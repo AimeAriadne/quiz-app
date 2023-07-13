d285e55a4fe1cf2533ae74f2b9d811518c6bb5a6
Aime Ramalho <aimeariadne@hotmail.com>
2023-07-13 10:16:34 +0100

    create quiz form

---
+++ src/components/organisms/QuizForm.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import QuizTitleText from '@/components/atoms/QuizTitleText.vue'
+import QuizOptions from '@/components/molecules/QuizOptions.vue'
+import QuizBottom from '@/components/molecules/QuizBottom.vue'
+import { storeToRefs } from 'pinia'
+
+import { useQuizStore } from '@/stores/quiz'
+
+const { question } = storeToRefs(useQuizStore())
+const { handleSubmission } = useQuizStore()
+
+</script>
+
+<template>
+  <form 
+    class="quiz-form"
+    @submit.prevent="handleSubmission"  
+  >
+    <QuizTitleText :text="question.question"/>
+
+    <QuizOptions/>
+
+    <QuizBottom/>
+  </form>
+</template>
+
--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,10 +1,25 @@
 <script setup lang="ts">
-import QuizBottom from '@/components/molecules/QuizBottom.vue'
+import QuizForm from '@/components/organisms/QuizForm.vue'
+import { storeToRefs } from 'pinia'
+import { useQuizStore } from '@/stores/quiz'
+
+const { loading } =  storeToRefs(useQuizStore())
 
 </script>
 
 <template>
-  <main>
-    <QuizBottom/>
+  <main class="quiz-view">
+    <div v-if="loading">
+      Loading Quiz...
+    </div>
+    <QuizForm v-else/>
   </main>
 </template>
+
+<style scoped>
+.quiz-view {
+  @apply
+    w-6/12
+    xl:w-3/12
+}
+</style>
