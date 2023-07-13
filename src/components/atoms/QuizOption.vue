9d644eb25dbd831c7ef9994785adbbcf63d8dc93
Aime Ramalho <aimeariadne@hotmail.com>
2023-07-13 13:50:13 +0100

    fix option data binding

--- src/components/atoms/QuizOption.vue
+++ src/components/atoms/QuizOption.vue
@@ -1,24 +1,35 @@
 <script setup lang="ts">
+import { computed } from 'vue'
+
 const props = defineProps<{
   option: string,
   disabled?: boolean,
-  modelValue: string | null
+  modelValue: string | null, 
 }>()
 
 const emit = defineEmits<{
-  (e: 'update:modelValue', value: string): void
+  (e: 'update:modelValue', value: string | null): void
 }>()
+
+const value = computed({
+  get() {
+    return props.modelValue
+  },
+  set(value) {
+    emit('update:modelValue', value)
+  }
+})
 </script>
 
 <template>
   <div class="quiz-option">
     <input 
       :id="option"
-      :value="modelValue"
+      :value="option"
       :disabled="disabled"
       type="radio"
       name="option"  
-      @change="emit('update:modelValue', option)"
+      v-model="value"
     >
 
     <label :for="option">{{ option }}</label>
